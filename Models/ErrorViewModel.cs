06c614d486e0299ea2fa9211869706426cf98675
Suyog-Kulkarni <kulkarnisuyog192@gmail.com>
2024-09-10 13:10:09 +0530

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace For_Git.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
